e2ce23fd03bb21365cc11846274927c0812fcbef
Monique Silva <moniquesntsilva@gmail.com>
2024-04-16 22:23:44 -0300

    feat: structure

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center bg-[hsl(30,_54%,_90%)]">
+      Hello world!
+    </main>
+  );
+}
